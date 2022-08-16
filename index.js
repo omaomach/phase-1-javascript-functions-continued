c96f832090c6d58612455e2da16a6084df57584f
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-08-16 23:26:19 +0300

    completed javascript functions continued assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+
+}
+
+console.log(saturdayFun("bathe my dog"));
+
+const mondayWork = function(activity = 'go to the office') {
+
+    return `This Monday, I will ${activity}.`;
+}
+
+console.log(mondayWork('code'));
+
+function wrapAdjective(one = '*') {
+    return function(two = 'special') {
+
+        return `You are ${one}${two}${one}!`;
+    } 
+}
+
+console.log(wrapAdjective('%')('a dedicated programmer'));
